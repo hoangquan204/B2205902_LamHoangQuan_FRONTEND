abd90209d47ce48186dca86be516412d11cce740
hoangquan204 <quanpogba888@gmail.com>
2025-09-26 20:16:11 +0700

    Khoi tao du an

---
+++ B2205902_LamHoangQuan_FRONTEND/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page">
+    <h1>Hello, Vue.js!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloPage",
+};
+</script>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
